e02bd85dc85296355531eeda42e9f79178c71135
Evg P <6857762+evgp@users.noreply.github.com>
2023-04-27 13:10:10 +0300

    Added colors

--- wgshow.sh
+++ wgshow.sh
@@ -3,13 +3,16 @@
 WG_CONFIG=/etc/wireguard/wg0.conf
 WG_SHOW=$(wg show)
 IFS=$'\n'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No color
 
 for line in $WG_SHOW 
 do
         if echo $line | grep -q "peer"; then
                 PEER=$(echo $line | awk '{print $NF}')
                 COMMENT=$(cat $WG_CONFIG | grep -B 2 $PEER | head -n 1 | awk '{print $NF}')
-                echo $line \# $COMMENT
+                echo -e "${GREEN} ${COMMENT} ${NC} ${YELLOW} ${line} ${NC}"
         else
                 echo $line
         fi
